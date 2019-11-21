iarenaza/moodle-filter_multilang2

commit 8c29e07158cb16f0369370b5260119866c411cec

Avoid unnecessary code duplication
There's no need for two regexp matching calls (with the same regexp!)
using two different callbacks. We can simply check in the callback
itself if we performed any substitution. And if we didn't, check
whether the language specified in the lang block is 'other', and act
accordingly.

filter.php
@@ -64,26 +64,23 @@ class filter_multilang2 extends moodle_text_filter {
      * @return string The filtered text for this multilang block.
      */
     public function filter($text, array $options = array()) {
-        $this->replacedone = false;
+
         if (stripos($text, 'mlang') === false) {
             return $text;
         }
 
-        $search = '/{mlang\s+([a-z0-9_-]+)\s*}(.*?){\s*mlang\s*}/is';
-        $result = preg_replace_callback($search, [$this, 'replace_callback'], $text);
         if (!isset(self::$parentcache)) {
             self::$parentcache = array();
         }
 
-        if ($this->replacedone == false) {
-            // Nothing has been replaced, because the current language hasn't been covered.
-            // Return the entry with 'other', if any is present.
-            $result = preg_replace_callback($search, [$this, 'replace_callback_other'], $text);
         $this->lang = current_language();
         if (!array_key_exists($this->lang, self::$parentcache)) {
             $parentlangs = get_string_manager()->get_language_dependencies($this->lang);
             self::$parentcache[$this->lang] = $parentlangs;
         }
+
+        $search = '/{mlang\s+([a-z0-9_-]+)\s*}(.*?){\s*mlang\s*}/is';
+        $result = preg_replace_callback($search, [$this, 'replace_callback'], $text);
 
         if (is_null($result)) {
             return $text; // Error during regex processing, keep original text.
@@ -94,7 +91,8 @@ class filter_multilang2 extends moodle_text_filter {
 
     /**
      * This function filters the current block of multilang tag. If the tag language
-     * matches the user current langauge (of its parent language), it returns the
+     * matches the user current langauge (or its parent languages), it returns the
+     * text of the block. Else if the tag language matches 'other', it returns the
      * text of the block. Otherwise it returns an empty string.
      *
      * @param array $langblock An array containing the matching captured pieces of the
@@ -109,28 +107,10 @@ class filter_multilang2 extends moodle_text_filter {
          */
         $blocklang = str_replace('-', '_', strtolower($langblock[1]));
         $blocktext = $langblock[2];
-        if (($blocklang == $mylang) || in_array($blocklang, $parentlangs)) {
-            $this->replacedone = true;
+        $parentlangs = self::$parentcache[$this->lang];
+        if (($blocklang == $this->lang) || in_array($blocklang, $parentlangs)) {
             return $blocktext;
-        }
-        return '';
-    }
-
-    /**
-     * This function filters the current block of multilang tag to return the
-     * strings with language 'other' for fallback. If the tag language
-     * matches 'other', it returns the text of the block. Otherwise it returns
-     * an empty string.
-     *
-     * @param array $langblock An array containing the matching captured pieces of the
-     *                         regular expression. They are the language of the tag,
-     *                         and the text associated with that language.
-     * @return string
-     */
-    protected function replace_callback_other($langblock) {
-        $blocklang = strtolower($langblock[1]);
-        $blocktext = $langblock[2];
-        if ($blocklang == 'other') {
+        } elseif ($blocklang == 'other') {
             return $blocktext;
         }
         return '';

commit 0cb8610b56043f6723945429b158b29bf40a9e72

Fix breakage introduced in commit 8c29e07
Even if we had some unit tests in place, they were not sufficient to
catch the breakage introduced in commit 8c29e07. The behavior
introduced in that commit is not compatible with the intended behavior
of the fallback 'other' language tag. They behave identically only in
very simple cases (like some of the unit tests we had), but not in the
general case.

The root of the problem is that the callback function is called for
each langblock separately. So we don't know what happened in other
langblocks, and we can't decide whether to fallback to language
'other'. Thus, it is not possible to do a single
preg_replace_callback() operation. We need to do it once for the
user active language. And only after all the langblocks were processed we
can decide if we need to do it a second time (if no langblock matched)
for the 'other' language.

But we can at least avoid duplicating the replace callback function if
we refactor it to accept the filtering language, and call it with
either the user active language, or 'other'.

Closes #15

filter.php
@@ -76,13 +76,14 @@ class filter_multilang2 extends moodle_text_filter {
         }
 
         if (!isset(self::$parentcache)) {
-            self::$parentcache = array();
+            self::$parentcache['other'] = array();
         }
 
-        $this->lang = current_language();
-        if (!array_key_exists($this->lang, self::$parentcache)) {
-            $parentlangs = get_string_manager()->get_language_dependencies($this->lang);
-            self::$parentcache[$this->lang] = $parentlangs;
+        $this->replacementdone = false;
+        $currlang = current_language();
+        if (!array_key_exists($currlang, self::$parentcache)) {
+            $parentlangs = get_string_manager()->get_language_dependencies($currlang);
+            self::$parentcache[$currlang] = $parentlangs;
         }
 
         $search = '/{\s*mlang\s+(                               # Look for the leading {mlang
@@ -94,51 +95,65 @@ class filter_multilang2 extends moodle_text_filter {
                    (.*?)                                        # Now capture the text to be filtered.
                    {\s*mlang\s*}                                # And look for the trailing {mlang}.
                    /isx';
-        $result = preg_replace_callback($search, [$this, 'replace_callback'], $text);
 
+        $replacelang = $currlang;
+        $result = preg_replace_callback($search,
+                                        function ($matches) use ($replacelang) {
+                                            return $this->replace_callback($replacelang, $matches);
+                                        },
+                                        $text);
         if (is_null($result)) {
             return $text; // Error during regex processing, keep original text.
-        } else {
+        }
+        if ($this->replacementdone) {
             return $result;
         }
+
+        $replacelang = 'other';
+        $result = preg_replace_callback($search,
+                                        function ($matches) use ($replacelang) {
+                                            return $this->replace_callback($replacelang, $matches);
+                                        },
+                                        $text);
+        if (is_null($result)) {
+            return $text;
+        }
+        return $result;
     }
 
     /**
      * This function filters the current block of multilang tag. If
      * any of the tag languages (or their parent languages) match the
-     * user current language, it returns the text of the block. Else
-     * if the tag languages contain 'other', it returns the text of the
+     * specified filtering language, it returns the text of the
      * block. Otherwise it returns an empty string.
      *
+     * @param string $replacelang A string that specifies the language used to
+     *                            filter the matches.
      * @param array $langblock An array containing the matching captured pieces of the
      *                         regular expression. They are the languages of the tag,
      *                         and the text associated with those languages.
      * @return string
      */
-    protected function replace_callback($langblock) {
-
+    protected function replace_callback($replacelang, $langblock) {
         /* Normalize languages. We can use strtolower instead of
          * core_text::strtolower() as language short names are ASCII
          * only, and strtolower is much faster. We have to remove the
          * white space between language names to be able to match them
-         * to official langguage names.
+         * to official language names.
          */
         $blocklangs = explode(',', str_replace(' ', '', str_replace('-', '_', strtolower($langblock[1]))));
         $blocktext = $langblock[2];
-        $parentlangs = self::$parentcache[$this->lang];
+        $parentlangs = self::$parentcache[$replacelang];
         foreach ($blocklangs as $blocklang) {
-            /* We don't check for empty values of $lang as they simply don't
+            /* We don't check for empty values of $blocklang as they simply don't
              * match any language and they don't produce any errors or warnings.
              */
-            if (($blocklang === $this->lang) || in_array($blocklang, $parentlangs)) {
+            if (($blocklang === $replacelang) || in_array($blocklang, $parentlangs)) {
+                $this->replacementdone = true;
                 return $blocktext;
             }
         }
 
-        if (in_array('other', $blocklangs)) {
-            return $blocktext;
-        } else {
-            return '';
-        }
+        return '';
     }
 }

tests/filter_test.php
@@ -184,6 +184,33 @@ class filter_multilang2_testcase extends advanced_testcase {
                 'after' => 'Other language Middle Other language',
             ),
             array (
+                'filterwithlang' => 'fr',
+                'before' => '{mlang other}Hello!{mlang}{mlang es,es_mx}¡Hola!{mlang}
+                             This text is common for all languages because it is outside of all lang blocks.
+                             {mlang other}Bye!{mlang}{mlang it}Ciao!{mlang}',
+                'after'  => 'Hello!
+                             This text is common for all languages because it is outside of all lang blocks.
+                             Bye!',
+            ),
+            array (
+                'filterwithlang' => 'es_mx',
+                'before' => '{mlang other}Hello!{mlang}{mlang es,es_mx}¡Hola!{mlang}
+                             This text is common for all languages because it is outside of all lang blocks.
+                             {mlang other}Bye!{mlang}{mlang it}Ciao!{mlang}',
+                'after'  => '¡Hola!
+                             This text is common for all languages because it is outside of all lang blocks.
+                             ',
+            ),
+            array (
+                'filterwithlang' => 'it',
+                'before' => '{mlang other}Hello!{mlang}{mlang es,es_mx}¡Hola!{mlang}
+                             This text is common for all languages because it is outside of all lang blocks.
+                             {mlang other}Bye!{mlang}{mlang it}Ciao!{mlang}',
+                'after'  => '
+                             This text is common for all languages because it is outside of all lang blocks.
+                             Ciao!',
+            ),
+            array (
                 'filterwithlang' => 'es',
                 'before' => '{mlang en,fr,es}Todo el texto está en español{mlang}',
                 'after'  => 'Todo el texto está en español',