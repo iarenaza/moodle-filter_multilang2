8558f5f2492647366ac0827c7a6b1c3ff24beede
Iñaki Arenaza <iarenaza@escomposlinux.org>
2026-04-12 12:33:53 +0200

    Enhance and fix configuration setting help texts
    Some of the descriptions and examples on how the filter works were
    wrong! Someone said that the initial descriptions were confusing and
    used ChatGPT to propose an easier to understand description.
    
    We accepted the proposal, and didn't thoroughly review it. If we had
    done it, we would have seen the proposal included descriptions that
    were wrong (they didn't match the way the filter actually works).
    
    We hope that the new descriptions and examples are easy the
    understand. At least we now know for certain that what they say match
    how the filter works.

--- lang/en/filter_multilang2.php
+++ lang/en/filter_multilang2.php
@@ -30,68 +30,141 @@ $string['parentlangalwaysen'] = 'Always use parent languages, including \'en\'.'
 $string['parentlangbehaviour'] = 'Parent languages behaviour';
 $string['parentlangbehaviour_desc'] = '
 <p>
-  The filter determines whether a language block should be displayed
-  based on the languages specified in the block and the current
-  language being used by the user ("the user\'s current
-  language"). This matching process can follow three different
-  approaches, known as "<em>parent languages behaviour</em>":
+  In the descriptions below, "the user\'s active language" refers to
+  the language that the user has configured as the active browsing
+  language at a given moment. It can be the language that the user
+  has configured in his or her profile, or the language that the
+  user has temporarily set by choosing a different browsing language
+  from Moodle\'s language menu.
+</p>
+<p>
+  The descriptions below also use the term "language block". A
+  language block is any content starting with a <code>{mlang
+  XX}</code> tag (or <code>{mlang XX, YY, ZZ}</code>`, etc.), and
+  ending with a <code>{mlang}</code> tag.
+</p>
+<p>
+  The filter determines whether a text in a language block should be
+  displayed, or not, based on the language(s) specified in the
+  language block, and the user\'s active language. This matching
+  process can follow different approaches, known as "<em>parent
+  languages behaviour</em>". There are three possible behaviours:
 </p>
 <ol>
   <li>
-    <b>Always use parent languages, excluding \'en\'.</b>
-    <ul>
-      <li>
-        This is the default setting. The filter considers the
-        languages listed in the language block\'s <code>{mlang
-        ...}</code> tag, and all of their parent languages (up to,
-        but not including, the root <code>en</code> language).
-      </li>
-      <li>
-        Example: If a language block specifies <code>{mlang
-        en_us_k12}</code>, it will only display if the user\'s
-        current language is <code>en_us_k12</code>
-        or <code>en_us</code> but not <code>en</code>.
-      </li>
-      <li>
-        Note: English can still be used explicitly in the language
-        block. For example, <code>{mlang en}This text will be shown
-        when the user’s current language is \'en\'.{mlang}</code>
-        will display the content when the user\'s current language
-        is <code>en</code>.
-      </li>
-    </ul>
+    <p>
+      <b>Always use parent languages, excluding \'en\'.</b>
+    </p>
+    <p>
+      This is the default setting, and the way the filter
+      traditionally worked before. The filter takes the user\'s active
+      language (e.g., \'<code>en_us_kids12</code>\') and computes its
+      parent languages list. When computing the parent languages list,
+      \'<code>en</code>\' is never included in that list.
+    </p>
+    <p>
+      E.g, for \'<code>en_us_kids12</code>\', the parent languages
+      list would only include \'<code>en_us</code>\', even if
+      \'<code>en</code>\' is the parent language of
+      \'<code>en_us</code>\'. Because \'<code>en</code>\' is always
+      excluded from the parent languages list in this case. But if the
+      user\'s active language was \'<code>fr_ca_kids12</code>\', the
+      parent languages list would include \'<code>fr_ca</code>\' and
+      \'<code>fr</code>\'.
+    </p>
+    <p>
+      The filter then checks if any of the languages specified in the
+      language block match either the user\'s active language, or any
+      of the languages in the parent languages list. If any match is
+      found, the language block is displayed. If no match is found,
+      the language block is not displayed.
+    </p>
+    <p>
+      <b>Example 1</b>: If the user\'s active language is
+      \'<code>en_us_kids12</code>\', the parent languages list will
+      include \'<code>en_us</code>\' only (as stated above). Thus, a
+      language block like <code>{mlang en_us_kids12}Some content in
+      en_us_kids12{mlang}</code> will be displayed. And so will a
+      language block like <code>{mlang en_us}Some content in
+      eu_us{mlang}</code>. But a language block like <code>{mlang
+      en}Some content in en{mlang}</code> will not be displayed.
+      Because \'<code>en</code>\' is excluded from the parent
+      languages list, and thus it will neither match the user\'s
+      active language, nor any language in parent languages list.
+    </p>
+    <p>
+      <b>Example 2</b>: If the user\'s active language is
+      \'<code>fr_ca_kids12</code>\', the parent languages list will
+      include \'<code>fr_ca</code>\' and \'<code>fr</code>\'. Thus, a
+      language block like <code>{mlang fr_ca_kids12}Some content in
+      fr_ca_kids12{mlang}</code> will be displayed. And so will a
+      language block like <code>{mlang fr_ca}Some content in
+      fr_ca{mlang}</code>, and a language block like <code>{mlang
+      fr}Some content in fr{mlang}</code>.
+    </p>
+    <p>
+      <b>Note</b>: English can still be used explicitly in the
+      language block. But the text of that language block will only be
+      displayed when the user\'s active language is exactly
+      \'<code>en</code>\'.
+    </p>
   </li>
   <li>
-    <b>Always use parent languages, including \'en\'.</b>
-    <ul>
-      <li>
-        This setting works like the previous one, but includes the
-        root <code>en</code> as a valid parent language.
-      </li>
-      <li>
-        Example: If a language block specifies <code>{mlang
-        en_us_k12}</code>, it will display if the user\'s current
-        language is either <code>en_us_k12</code>,
-        <code>en_us</code> or <code>en</code>.
-      </li>
-    </ul>
+    <p>
+      <b>Always use parent languages, including \'en\'.</b>
+    </p>
+    <p>
+      This setting works like the previous one, but it does not
+      exclude the \'<code>en</code>\' language from the list of valid
+      parent languages of the user\'s active language.
+    </p>
+    <p>
+      <b>Example 1</b>: If the user\'s active language is
+      \'<code>en_us_kids12</code>\', the parent languages list will
+      include \'<code>en_us</code>\' and \'<code>en</code>\' (as in
+      this case \'<code>en</code>\' will not be excluded from the list
+      of valid parent languages). Thus, a language block
+      like <code>{mlang en_us_kids12}Some content in
+      en_us_kids12{mlang}</code> will be displayed. And so will
+      language blocks like <code>{mlang en_us}Some content in
+      en_us{mlang}</code> or <code>{mlang en}Some content in
+      en{mlang}</code>.
+    </p>
+    <p>
+      <b>Example 2</b>: If the user\'s active language is
+      \'<code>fr_ca_kids12</code>\', the parent languages list will
+      include \'<code>fr_ca</code>\' and \'<code>fr</code>\'. Thus, a
+      language block like <code>{mlang fr_ca_kids12}Some content in
+      fr_ca_kids12{mlang}</code> will be displayed. And so will
+      language blocks like <code>{mlang fr_ca}Some content in
+      fr_ca{mlang}</code> or <code>{mlang fr}Some content in
+      fr{mlang}</code>. But a language block like <code>{mlang en}Some
+      content in fr{mlang}</code> will <b>not</b> be
+      displayed. Because in this case, the \'<code>en</code>\'
+      language is not a parent language of
+      \'<code>fr_ca_kids12</code>\' (only \'<code>fr_ca</code>\' and
+      \'<code>fr</code>\' are).
+    </p>
   </li>
   <li>
-    <b>Never use parent languages.</b>
-    <ul>
-      <li>
-        As the name suggests, no parent languages are used. The
-        filter only matches the languages explicitly listed in the
-        language block, without considering any parent languages.
-      </li>
-      <li>
-        Example: If a language block specifies <code>{mlang
-        en_us_k12}</code>, it will only display if the user\'s
-        current language is <code>en_us_k12</code>,
-        not <code>en_us</code>
-        or <code>en</code>.
-      </li>
-    </ul>
+    <p>
+      <b>Never use parent languages.</b>
+    </p>
+    <p>
+      As the name suggests, no parent languages are ever used. The
+      filter only tries to match the language(s) listed in the
+      language block to the user\'s current language, without
+      considering any parent languages at all.
+    </p>
+    <p>
+      <b>Example</b>: If the user\'s active language is
+      \'<code>fr_ca_kids12</code>\', a language block
+      like <code>{mlang fr_ca_kids12}Some content in
+      fr_ca_kids12{mlang}</code> will be displayed. But language
+      blocks like <code>{mlang fr_ca}Some content in
+      fr_ca{mlang}</code> or <code>{mlang fr}Some content in
+      fr{mlang}</code> will not be displayed.
+    </p>
   </li>
 </ol>';
 $string['parentlangdefault'] = 'Always use parent languages, excluding \'en\' (traditional behaviour).';
